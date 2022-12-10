5ffac6e8cfec4c57327c3b1b77e73c5dd92c40bc
Łukasz Kluska <lukaszklusk@gmail.com>
2022-12-10 11:06:13 +0100

    Add Employee and Role data models, configure H2 database

---
+++ src/main/java/pl/edu/agh/to/kinofilmy/Employee/Employee.java
@@ -0,0 +1,71 @@
+package pl.edu.agh.to.kinofilmy.Employee;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private Long roleId;
+
+    private String email;
+
+    private String phoneNumber;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
