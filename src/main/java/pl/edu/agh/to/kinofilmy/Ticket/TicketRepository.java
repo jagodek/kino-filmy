8620a91b606cef2ea988a82db5979e5554c56548
Łukasz Kluska <lukaszklusk@gmail.com>
2022-12-10 12:19:23 +0100

    Remove TicketRepository.java

--- src/main/java/pl/edu/agh/to/kinofilmy/Ticket/TicketRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.agh.to.kinofilmy.Ticket;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
