f83d2975dcde1898f4e79c6a968b7da6089705c1
Łukasz Kluska <lukaszklusk@gmail.com>
2022-12-10 10:33:33 +0100

    Initial commit, generated project with Spring Initializr

---
+++ src/main/java/pl/edu/agh/to/kinofilmy/KinoFilmyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.kinofilmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoFilmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoFilmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/to/kinofilmy/KinoFilmyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.kinofilmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoFilmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
