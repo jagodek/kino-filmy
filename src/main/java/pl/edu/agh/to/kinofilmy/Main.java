d56adbe821dab40241f21725e57325c0a30325b7
Łukasz Kluska <lukasz.kluska@comarch.pl>
2022-12-29 14:50:24 +0100

    Refactor model + JavaFX window 2

---
+++ src/main/java/pl/edu/agh/to/kinofilmy/Main.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to.kinofilmy;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(KinoFilmyApplication.class);
+    }
+}
