7f853e734628658cb2974d4b25e26f341acdbef4
Andrzej Starzyk <406772@student.agh.edu.pl>
2023-01-02 22:30:01 +0100

    fix some problems after unsuccessful push operation

---
+++ src/main/java/pl/edu/agh/to/kinofilmy/controllers/MainController.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.to.kinofilmy.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class MainController {
+
+    private final KinoFilmyApplicationController applicationController;
+
+    private Stage mainStage;
+
+
+    public MainController(KinoFilmyApplicationController applicationController) {
+        this.applicationController = applicationController;
+    }
+
+    public void setMainStage(Stage mainStage) {
+        this.mainStage = mainStage;
+    }
+
+    @FXML
+    private Button userManagementButton;
+
+    @FXML
+    public void handleManageUsersAction(ActionEvent event){
+        applicationController.showUserManagement(mainStage);
+    }
+}
---
+++ src/main/java/pl/edu/agh/to/kinofilmy/model/Roles/RolesService.java
@@ -0,0 +1,18 @@
+package pl.edu.agh.to.kinofilmy.model.roles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.to.kinofilmy.model.employee.Employee;
+
+import java.util.Optional;
+
+@Service
+public class RolesService {
+
+    @Autowired
+    private RolesRepository repository;
+
+    public Optional<Roles> getRole(String name){
+        return repository.findRolesByRoleName(name);
+    }
+}
