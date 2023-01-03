c2fc518081abe280e0da2e2787f3671167048023
jagodek <mchlgdk@gmail.com>
2023-01-04 00:23:17 +0100

    Statistics work

--- src/main/java/pl/edu/agh/to/kinofilmy/model/film/FilmRepository.java
+++ src/main/java/pl/edu/agh/to/kinofilmy/model/film/FilmRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface FilmRepository extends JpaRepository<Film, Long> {
     public boolean existsFilmById(long id);
 
-    @Query("SELECT m, COUNT(t) FROM Film m INNER JOIN Ticket t WHERE t.saleDate = :date GROUP BY m ORDER BY COUNT(t) DESC")
+    @Query("SELECT m, COUNT(t) FROM Film m INNER JOIN Showing s ON s.film = m inner join Ticket t on t.showing = s WHERE t.saleDate = :date GROUP BY m.id ORDER BY COUNT(t.id) DESC")
     List<Object[]> findFilmsByTicketsSoldOnDate(@Param("date") Date date);
 }
