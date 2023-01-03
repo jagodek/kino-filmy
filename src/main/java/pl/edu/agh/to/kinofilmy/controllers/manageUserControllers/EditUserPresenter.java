6ea6b6dd12f7f24e2080e8a624699f79239e33cd
jagodek <mchlgdk@gmail.com>
2023-01-03 15:07:22 +0100

    edit user validation, minor cosmetic changes

--- src/main/java/pl/edu/agh/to/kinofilmy/controllers/manageUserControllers/EditUserPresenter.java
+++ src/main/java/pl/edu/agh/to/kinofilmy/controllers/manageUserControllers/EditUserPresenter.java
@@ -4,8 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import pl.edu.agh.to.kinofilmy.controllers.KinoFilmyApplicationController;
@@ -16,6 +18,7 @@ import pl.edu.agh.to.kinofilmy.model.roles.RolesService;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Controller
 public class EditUserPresenter {
@@ -53,6 +56,21 @@ public class EditUserPresenter {
     @FXML
     private Button submit;
 
+    @FXML
+    private Label firstNameError;
+    @FXML
+    public Label lastNameError;
+    @FXML
+    public Label usernameError;
+    @FXML
+    public Label passwordError;
+    @FXML
+    public Label emailError;
+    @FXML
+    public Label phoneError;
+    @FXML
+    public Label rolesError;
+
 
     public void setStage(Stage stage) {
         this.editUserStage = stage;
@@ -77,8 +95,184 @@ public class EditUserPresenter {
             this.roleInput.setValue(this.roleInput.getValue());
         }));
 
+        this.firstNameInput.textProperty().addListener((observable, oldValue, newValue) -> {
+                Pair<Boolean,String> err = isNameValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.firstNameError.setText(msg);
+                }
+                else{
+                    this.firstNameError.setText("");
+                }
+
+            setButton();
+
+        });
 
 
+       this.lastNameInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            Pair<Boolean,String> err = isNameValid(newValue);
+            boolean valid = err.getKey();
+            String msg = err.getValue();
+            if(!valid){
+                this.lastNameError.setText(msg);
+            }
+            else{
+                this.lastNameError.setText("");
+            }
+
+            setButton();
+       });
+
+
+        this.roleInput.valueProperty().addListener((observable, oldValue, newValue) -> {
+                  Pair<Boolean,String> err = isRolesValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.rolesError.setText(msg);
+                }
+                else{
+                    this.rolesError.setText("");
+                }
+            setButton();
+
+        });
+        this.usernameInput.textProperty().addListener((observable, oldValue, newValue) -> {
+                Pair<Boolean,String> err = isUsernameValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.usernameError.setText(msg);
+                }
+                else{
+                    this.usernameError.setText("");
+                }
+            setButton();
+        });
+
+
+        this.passwordInput.textProperty().addListener((observable, oldValue, newValue) -> {
+                Pair<Boolean,String> err = isPasswordValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.passwordError.setText(msg);
+                }
+                else{
+                    this.passwordError.setText("");
+                }
+            setButton();
+        });
+
+
+        this.emailInput.textProperty().addListener((observable, oldValue, newValue) -> {
+                Pair<Boolean,String> err = isEmailValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.emailError.setText(msg);
+                }
+                else{
+                    this.emailError.setText("");
+                }
+            setButton();
+        });
+
+
+        this.phoneInput.textProperty().addListener((observable, oldValue, newValue) -> {
+                Pair<Boolean,String> err = isPhoneValid(newValue);
+                boolean valid = err.getKey();
+                String msg = err.getValue();
+                if(!valid){
+                    this.phoneError.setText(msg);
+                }
+                else{
+                    this.phoneError.setText("");
+                }
+
+            setButton();
+        });
+
+    }
+
+
+    private void setButton(){
+        if(isNameValid(this.firstNameInput.getText()).getKey() &&
+                isNameValid(this.lastNameInput.getText()).getKey() &&
+                isRolesValid(this.roleInput.getValue()).getKey() &&
+                isUsernameValid(this.usernameInput.getText()).getKey() &&
+                isPasswordValid(this.passwordInput.getText()).getKey() &&
+                isEmailValid(this.emailInput.getText()).getKey() &&
+                isPhoneValid(this.phoneInput.getText()).getKey()
+
+        ){
+            this.submit.setDisable(false);
+        }
+        else
+            this.submit.setDisable(true);
+    }
+
+
+    private Pair<Boolean,String> isRolesValid(Roles role){
+        if(role == null){
+            return new Pair<>(false,"role required");
+        }
+        return new Pair<>(true,"");
+    }
+    private Pair<Boolean,String> isNameValid(String string){
+        if(string.equals("")){
+            return new Pair<>(false,"name required");
+        }
+        else if(!string.matches("[A-ZĄŁÓŻŹĆĘŚŃ]{1}[a-zęóąśłżźćń]*"))
+        {
+            return  new Pair<>(false,"Wrong name format");
+        }
+        return new Pair<>(true,"");
+    }
+
+    private Pair<Boolean,String> isUsernameValid(String string){
+        if(string.equals("")){
+            return new Pair<>(false,"username required");
+        }
+        else if(!string.matches("[A-ZĄŁÓŻŹĆĘŚŃa-zęóąśłżźćń_$]{1}.*"))
+        {
+            return  new Pair<>(false,"Wrong name format");
+        }
+        return new Pair<>(true,"");
+    }
+
+    private Pair<Boolean,String> isPasswordValid(String string){
+        if(string.equals("")){
+            return new Pair<>(false,"password required, do i have to say it?");
+        }
+        else if(!string.matches(".{6}.+"))
+        {
+            return  new Pair<>(false,"Password too short");
+        }
+        return new Pair<>(true,"");
+    }
+
+    private Pair<Boolean,String> isEmailValid(String string){
+        if(string.equals("")){
+            return new Pair<>(false,"email required");
+        }
+        else if(!string.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
+        {
+            return  new Pair<>(false,"Wrong format of email");
+        }
+        return new Pair<>(true,"");
+    }
+
+    private Pair<Boolean, String> isPhoneValid(String string){
+        if(string.equals("")){
+            return new Pair<>(false,"Phone number is required");
+        }
+        else if(!string.matches("^([+]?([0-9]{1}[ ]?){3})?([0-9]{1}[\\s-]*){9}$")){
+            return new Pair<>(false,"wrong phone number format");
+        }
+        return new Pair<>(true,"");
     }
 
     @FXML
