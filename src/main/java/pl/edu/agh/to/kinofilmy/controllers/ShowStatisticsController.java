d3842774f49b9f08fc3aed1cc3e43e7f8228ac17
jagodek <mchlgdk@gmail.com>
2023-01-17 20:35:45 +0100

    basic statistics work added lotr trilogy to json

--- src/main/java/pl/edu/agh/to/kinofilmy/controllers/ShowStatisticsController.java
+++ src/main/java/pl/edu/agh/to/kinofilmy/controllers/ShowStatisticsController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Controller;
 import pl.edu.agh.to.kinofilmy.model.film.FilmService;
@@ -40,31 +41,60 @@ public class ShowStatisticsController {
 
 
 
-    private String[] timePeriodsNames = {"This Day","This Week","This Month","This Year"};
+    private String[] statisticTypeNames = {"Films by tickets sold","Most watched genres","Most watched director"};
+    private String[] timePeriodsNames = {"This Day","This Month","This Year"};
 
 
     public ShowStatisticsController(FilmService filmService){
         this.filmService = filmService;
     }
+
     @FXML
     public void initialize(){
-        this.TypeOfStatisticsChoiceBox.getItems().add("Films by tickets sold");
+        this.place.setCellValueFactory(new PropertyValueFactory<>("Place"));
+        this.title.setCellValueFactory(new PropertyValueFactory<>("String"));
+        this.ticketsSold.setCellValueFactory(new PropertyValueFactory<>("ticketsSold"));
+        for (String val :
+                statisticTypeNames) {
+            this.TypeOfStatisticsChoiceBox.getItems().add(val);
+        }
+
         for (String val :
                 timePeriodsNames) {
             this.TimePeriodChoiceBox.getItems().add(val);
         }
+        this.TypeOfStatisticsChoiceBox.setOnAction((event -> {
+            choosenType();
+        }));
+        this.TimePeriodChoiceBox.setOnAction((event -> {
+            choosenType();
+        }));
         this.TypeOfStatisticsChoiceBox.setValue(this.TypeOfStatisticsChoiceBox.getItems().get(0) );
         this.TimePeriodChoiceBox.setValue(this.TimePeriodChoiceBox.getItems().get(0) );
-        choosenType(this.TypeOfStatisticsChoiceBox.getValue());
+        choosenType();
+
     }
 
     public void setShowStatisticsStage(Stage showStatisticsStage) {
         this.showStatisticsStage = showStatisticsStage;
     }
 
-    public void choosenType(String type){
-        if(this.TimePeriodChoiceBox.getValue() == "This Day"){
-            this.filmList = this.filmService.getMoviesByTicketsSold();
+    public void choosenType(){
+        if(this.TypeOfStatisticsChoiceBox.getValue() == "Films by tickets sold") {
+            this.table.setItems(this.filmService.getMoviesByTicketsSold(this.TimePeriodChoiceBox.getValue()));
+            this.table.refresh();
+
+        }
+        if(this.TypeOfStatisticsChoiceBox.getValue() == "Most watched director") {
+            this.table.setItems(this.filmService.getMoviesByDirectorDay(this.TimePeriodChoiceBox.getValue()));
+            this.table.refresh();
+
+        }
+
+        if(this.TypeOfStatisticsChoiceBox.getValue() == "Most watched genres") {
+            this.table.setItems(this.filmService.getMoviesByGenre(this.TimePeriodChoiceBox.getValue()));
+            this.table.refresh();
+
         }
     }
 
